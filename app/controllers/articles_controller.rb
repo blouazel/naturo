dbc7315c39b1066cab970530b894e85a1ce641d1
briac louazel <briac@laposte.net>
2021-05-13 11:33:54 +0200

    Authorize unlogged users to access index and show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
 
   def new
     @article = Article.new
