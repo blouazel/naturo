ea34e165a368f460ec210dba02532bd8e7ccee06
briac louazel <briac@laposte.net>
2021-05-13 15:35:51 +0200

    pundit setup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Votre article a été crée avec succès !"
       redirect_to article_path(@article)
