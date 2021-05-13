7243dc3980f594047fbc93187dca13787e27efab
briac louazel <briac@laposte.net>
2021-05-13 22:46:23 +0200

    change email in config.mailer_sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'justine@just-o-naturel.com'
+  config.mailer_sender = 'justine.feuerstoss@just-o-naturel.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
