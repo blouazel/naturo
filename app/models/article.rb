364d44d5e9b36e284791df9aeecc261d904e2885
briac louazel <briac@laposte.net>
2021-05-16 20:09:07 +0200

    config active storage

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ApplicationRecord
+  has_rich_text :rich_body
+  #has_one_attached :photo
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
   validates :rich_body, presence: true, length: { minimum: 50 }
   belongs_to :user, optional: true
   belongs_to :category, optional: true
   belongs_to :subcategory
-  has_rich_text :rich_body
 end
