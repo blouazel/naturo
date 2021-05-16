31ba14f5b764ffe08645e84d86e21545f44fb1c8
briac louazel <briac@laposte.net>
2021-05-16 22:48:37 +0200

    add photo for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_rich_text :rich_body
-  #has_one_attached :photo
+  has_one_attached :photo
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
   validates :rich_body, presence: true, length: { minimum: 50 }
   belongs_to :user, optional: true
