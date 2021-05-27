2ac83f913fbc71a9adc341375015a51a8f87993e
briac louazel <briac@laposte.net>
2021-05-27 16:06:50 +0200

    remove category and subcategory from views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   #has_ancestry
-  #has_many :articles
-  has_many :subcategories
+  has_many :articles
+  #has_many :subcategories
 end
