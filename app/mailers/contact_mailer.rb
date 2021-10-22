0463accd2df7d07064f3907229d097c8d240ea8a
briac louazel <briac@laposte.net>
2021-10-22 10:14:11 +0200

    create view contact/html.erb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,7 @@
 class ContactMailer < ApplicationMailer
+  default from: "justine.feuerstoss@just-o-naturel.com"
+  def contact(message)
+    @message = message
+    mail(to: 'briac@rado2loc.com', subject: 'Nouveau contact sur just-o-naturel.com')
+  end
 end
