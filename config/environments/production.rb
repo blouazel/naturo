411246beef961aaef1cf3128c22d03f6673f0d54
briac louazel <briac@laposte.net>
2021-11-18 11:04:34 +0100

    change prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # 18-11-2021 Config for sending email with contact form:
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'just-o-naturel.com' }
+  config.action_mailer.default_url_options = { :host 'just-o-naturel.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -125,5 +125,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'just-o-naturel.com' }
+  #config.action_mailer.default_url_options = { host: 'just-o-naturel.com' }
+
 end
