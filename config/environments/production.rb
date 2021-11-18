200f2e9dac0c90773a3c437e1d3bf9c5fcef7d3f
briac louazel <briac@laposte.net>
2021-11-18 11:49:04 +0100

    uncomment line 8 9 config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "mail.gandi.net",
-  #  port: 587,
-  #  domain: "gandi.net",
+    port: 587,
+    domain: "gandi.net",
   #  authentication: "plain",
   #  enable_starttls_auto: true,
   #  user_name: MAIL_USERNAME,
