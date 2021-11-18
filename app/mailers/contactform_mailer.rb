e8737760627ddfd44fe243493795720fc395e27e
briac louazel <briac@laposte.net>
2021-11-18 14:13:26 +0100

    new inputs & reply to user

--- app/mailers/contactform_mailer.rb
+++ app/mailers/contactform_mailer.rb
@@ -1,8 +1,9 @@
 class ContactformMailer < ApplicationMailer
   default from: 'justine.feuerstoss@just-o-naturel.com'
-  def contactform(message)
+  def contactform(message, email, nom)
+    @nom = nom
     @message = message
-    #  @email = email
-    mail(to: 'justine.feuerstoss@just-o-naturel.com', subject: 'Nouveau message sur just-o-naturel.com')
+    @email = email
+    mail(to: 'justine.feuerstoss@just-o-naturel.com', subject: 'Nouveau message sur just-o-naturel.com', reply_to: @email)
   end
 end
