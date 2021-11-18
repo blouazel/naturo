51930ecc8232dbd31dc14be6ff4339424bf054f9
briac louazel <briac@laposte.net>
2021-11-18 11:31:04 +0100

    url options mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # 18-11-2021 Config for sending email with contact form:
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host 'just-o-naturel.com' }
+  config.action_mailer.default_url_options = { :host 'https://just-o-naturel.com' }
   #config.action_mailer.raise_delivery_errors = true
   #config.action_mailer.delivery_method = :smtp
   #config.action_mailer.smtp_settings = {
